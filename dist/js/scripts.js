06737807e5a3995eb3882ed801e114363d2b8e7a
David Miller <hello@davidmiller.io>
2021-04-28 20:33:24 -0400

    Bootstrap 4.6.0 and dev environment update

---
+++ dist/js/scripts.js
@@ -0,0 +1,7 @@
+/*!
+* Start Bootstrap - The Big Picture v4.3.0 (https://startbootstrap.com/template/the-big-picture)
+* Copyright 2013-2021 Start Bootstrap
+* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-the-big-picture/blob/master/LICENSE)
+*/
+// This file is intentionally blank
+// Use this file to add JavaScript to your project
