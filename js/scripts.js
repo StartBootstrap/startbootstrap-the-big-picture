9be5b67779f4514b1e39d187c4bad688e867dba9
David Miller <hello@davidmiller.io>
2021-06-29 13:33:35 -0400

    Bootstrap 5.0.2 update

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,5 @@
 /*!
-* Start Bootstrap - The Big Picture v5.0.1 (https://startbootstrap.com/template/the-big-picture)
+* Start Bootstrap - The Big Picture v5.0.2 (https://startbootstrap.com/template/the-big-picture)
 * Copyright 2013-2021 Start Bootstrap
 * Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-the-big-picture/blob/master/LICENSE)
 */
