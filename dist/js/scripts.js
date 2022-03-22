0fe059bdc9614d3bc11db98e5c2284287ab699f1
David Miller <hello@davidmiller.io>
2022-03-22 16:36:59 -0400

    dependencies update

--- dist/js/scripts.js
+++ dist/js/scripts.js
@@ -1,6 +1,6 @@
 /*!
 * Start Bootstrap - The Big Picture v5.0.4 (https://startbootstrap.com/template/the-big-picture)
-* Copyright 2013-2021 Start Bootstrap
+* Copyright 2013-2022 Start Bootstrap
 * Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-the-big-picture/blob/master/LICENSE)
 */
 // This file is intentionally blank
