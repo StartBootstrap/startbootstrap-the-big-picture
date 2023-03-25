080ddaeb2d24c3f12c5ce8056f87266ab48e79aa
David Miller <hello@davidmiller.io>
2023-03-25 18:17:40 -0400

    Bootstrap 5.2.3 dependency update

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,6 @@
 /*!
-* Start Bootstrap - The Big Picture v5.0.5 (https://startbootstrap.com/template/the-big-picture)
-* Copyright 2013-2022 Start Bootstrap
+* Start Bootstrap - The Big Picture v5.0.6 (https://startbootstrap.com/template/the-big-picture)
+* Copyright 2013-2023 Start Bootstrap
 * Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-the-big-picture/blob/master/LICENSE)
 */
 // This file is intentionally blank
